b3e688769b91192c40022af7b22e5e109c0f0d47
Andrew Hoyer <me@andrew-hoyer.com>
2011-04-25 23:02:38 -0700

    Fixes to the content iterator

--- js/contentiterator.js
+++ js/contentiterator.js
@@ -1,6 +1,6 @@
 
 var ContentIterator = function(element){
-    this.containerIndex = 0;
+    this.containerIndex = -1;
     this.containers = [].slice.call(element.search('*'));
     this.letterIndex = 0;
     this.letters = [];
@@ -8,14 +8,14 @@ var ContentIterator = function(element){
 
 ContentIterator.prototype = {
     
-    nextLetter: function(){
+    getLetter: function(){
         return this.letters[this.letterIndex++];
     },
     
-    nextContainer: function(){
+    getContainer: function(){
         var container = this.containers[++this.containerIndex];
         
-        this.letters = container.textContent.split('');
+        this.letters = container ? container.textContent.split('') : [];
         this.letterIndex = 0;
         
         return container;
